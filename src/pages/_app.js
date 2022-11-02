28c6de67fb16d1497ab4b7da5d3231bbfea56b64
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2022-11-01 22:01:27 -0400

    style: login page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css';
+import Login from './auth/login';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Login />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
---
+++ src/pages/auth/login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="h-screen flex flex-col bg-white">
+      <div className="bg-[#47ADF5]/30 h-[93%] flex justify-center items-center">
+        <div className="w-8/12 bg-white h-[400px] shadow-lg rounded-lg"></div>
+      </div>
+      <div className="bg-white h-[7%] flex justify-center items-center font-[poppins] text-[#1A579A] font-semibold text-sm">
+        © 2022 Todos los derechos reservados
+      </div>
+    </div>
+  );
+};
+
+export default Login;
