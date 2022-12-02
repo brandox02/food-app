ef12a8e7e99ce6d86904e6309aa07939bf9c0d6c
Brandon Fernandez <brandox02.dev@gmail.com>
2022-12-02 09:01:07 -0400

    api url extracted to .env.local file

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,14 +1,13 @@
 import {ApolloProvider as ApolloProviderSource, createHttpLink,ApolloClient, InMemoryCache } from '@apollo/client'
-
 const apolloClient = new ApolloClient({
-  uri: `http://localhost:3000/graphql`,
+  uri: process.env.NEXT_PUBLIC_API_URL + '/graphql',
   cache: new InMemoryCache({addTypename: false}),
   
-})
+});
 
 export const setAuthToken = (token) => {
   apolloClient.setLink( createHttpLink({
-    uri: 'http://localhost:3000/graphql',
+    uri: process.env.NEXT_PUBLIC_API_URL + '/graphql',
     headers: {
       'authorization': `Bearer ${token}`
     },
